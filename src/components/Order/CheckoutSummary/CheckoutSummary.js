e177cad548579c5d33573850d7a36127c03ccd3d
Martin Staykov <marti.staykov@gmail.com>
2020-03-28 21:57:38 +0200

    Changed template style for checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,12 +7,14 @@ const checkoutSummary= (props) => {
 
     return(
         <div className={classes.CheckoutSummary}>
-            <h1>We hope you like it</h1>
-            <div style={{width: '100%',  margin:'auto'}}>
-                <Burger ingredients={props.ingredients}/>
-                <h2>Price: {props.price}</h2>
+            <h1>This is your burger.</h1>
+            <h3> Would you like to continue the order.</h3>
+            <h2>Price: {props.price}</h2>
                 <Button className='mr-3 mt-4 btn-lg' onClick={props.CheckoutCancel}variant='danger'>Cancel</Button>
                 <Button className='mr-3 mt-4 btn-lg' onClick={props.CheckoutContinue}variant='primary'>Continue</Button>
+            <div style={{width: '100%',  margin:'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+               
             </div>
         </div>
     )
